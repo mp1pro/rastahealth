1c11fcbe2c8da67bb3847e7ae6dcb9c37cb728f9
mp1pro <mp1pro@gmail.com>
2017-04-21 14:23:22 -0400

    change release date for UI layout

--- views/Index.jsx
+++ views/Index.jsx
@@ -9,7 +9,7 @@ module.exports = React.createClass({
                 <p></p>
 				Current: <strong>Creating UI layout</strong>
                 <p></p>
-                ReleaseDate: <strong>April 08 2017</strong>
+                ReleaseDate: <strong>May 01 2017</strong>
             </div>
                 );
         }
