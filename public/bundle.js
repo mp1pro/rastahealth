815c4914d58dd73e4572e691bff88900f446d97b
mp1pro <mp1pro@gmail.com>
2017-05-13 14:18:05 -0400

    add indexjsx

--- public/bundle.js
+++ public/bundle.js
@@ -12531,28 +12531,29 @@ module.exports = React.createClass({
 var React = __webpack_require__(6);
 
 module.exports = React.createClass({
-        displayName: 'Index',
+    displayName: 'Index',
 
-        render: function () {
-                return React.createElement(
-                        'div',
-                        null,
-                        React.createElement('p', null),
-                        'Current: ',
-                        React.createElement(
-                                'strong',
-                                null,
-                                'Creating UI layout'
-                        ),
-                        React.createElement('p', null),
-                        'ReleaseDate: ',
-                        React.createElement(
-                                'strong',
-                                null,
-                                'May 01 2017'
-                        )
-                );
-        }
+    render: function () {
+        return React.createElement(
+            'article',
+            null,
+            React.createElement(
+                'div',
+                { className: 'article-title' },
+                'Home Remedies for Sore Throat'
+            ),
+            React.createElement(
+                'div',
+                { className: 'adsense-box' },
+                'desktop advert'
+            ),
+            React.createElement(
+                'span',
+                null,
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis dolor placerat erat consequat mattis. Pellentesque dui ante, lacinia fringilla diam vitae, suscipit laoreet eros. Quisque condimentum vitae tellus a aliquet. Phasellus auctor elit sit amet sem finibus placerat. Aenean vitae sapien pulvinar nisi finibus tempus et id purus. Fusce a orci eu nisi rhoncus fermentum. Donec eget turpis non nisl condimentum rhoncus et non diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna mi, faucibus eu tortor vitae, efficitur interdum turpis. Etiam lacus purus, laoreet sit amet erat ut, convallis sodales elit. Morbi et turpis eget magna volutpat posuere in non nibh. Aenean tempor tellus erat, id congue metus auctor sit amet. Morbi nec vestibulum lorem. Etiam eget congue ligula. In in neque id nulla dignissim tempor. Vestibulum scelerisque magna eu interdum vulputate. Maecenas gravida eget erat at semper. Phasellus blandit eu purus vel posuere. Donec nisi tortor, vestibulum pretium tortor ac, tempor ultricies velit. Mauris justo arcu, placerat quis erat quis, fringilla iaculis nisl. Pellentesque felis lacus, laoreet in aliquet a, blandit a nisl. Morbi scelerisque arcu sit amet nisi mollis fringilla. Sed libero nisi, porta non congue id, pretium id tellus. Nunc eu lorem nec sem faucibus pellentesque. Vivamus a ante imperdiet, euismod metus et, aliquet nisl. Fusce eu velit ut ligula vulputate pellentesque eu ut tellus. Vestibulum eget ex id orci pharetra placerat. Mauris vel felis auctor, viverra diam nec, mattis ante. Donec tincidunt vitae ex sit amet viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vitae vestibulum mi. Pellentesque faucibus, risus et mollis posuere, nisi ipsum vestibulum lacus, ac sodales orci purus in massa. Duis eu blandit mauris. Maecenas tincidunt magna ut quam porta luctus. Cras at nunc id tellus rutrum dignissim non ut purus. Etiam mollis enim sit amet nulla gravida, ut fermentum felis lacinia. Nam elementum urna odio, eget finibus ligula malesuada ac. Donec porttitor nulla eget semper vulputate. Quisque mollis maximus volutpat. Ut ultrices sagittis sollicitudin. Nunc vel maximus nisi, sit amet porttitor sem. Nam eleifend posuere turpis sit amet gravida. Pellentesque tristique auctor dictum. Sed molestie quis purus eget aliquet. Sed volutpat, felis ac vulputate volutpat, quam libero porta magna, sit amet viverra ante urna ac nisi. Sed at diam sit amet metus venenatis auctor vel iaculis nulla. Aenean vel egestas nunc, a cursus libero. Pellentesque accumsan nisl orci, rhoncus sollicitudin augue tempus congue. Sed eu velit rutrum, tincidunt justo non, tristique lorem. Nullam auctor mollis lobortis.'
+            )
+        );
+    }
 });
 
 /***/ }),
@@ -12821,25 +12822,7 @@ module.exports = React.createClass({
                             { className: 'share' },
                             'fb'
                         ),
-                        React.createElement(
-                            'article',
-                            null,
-                            React.createElement(
-                                'div',
-                                { className: 'article-title' },
-                                'Home Remedies for Sore Throat'
-                            ),
-                            React.createElement(
-                                'div',
-                                { className: 'adsense-box' },
-                                'desktop advert'
-                            ),
-                            React.createElement(
-                                'span',
-                                null,
-                                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis dolor placerat erat consequat mattis. Pellentesque dui ante, lacinia fringilla diam vitae, suscipit laoreet eros. Quisque condimentum vitae tellus a aliquet. Phasellus auctor elit sit amet sem finibus placerat. Aenean vitae sapien pulvinar nisi finibus tempus et id purus. Fusce a orci eu nisi rhoncus fermentum. Donec eget turpis non nisl condimentum rhoncus et non diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna mi, faucibus eu tortor vitae, efficitur interdum turpis. Etiam lacus purus, laoreet sit amet erat ut, convallis sodales elit. Morbi et turpis eget magna volutpat posuere in non nibh. Aenean tempor tellus erat, id congue metus auctor sit amet. Morbi nec vestibulum lorem. Etiam eget congue ligula. In in neque id nulla dignissim tempor. Vestibulum scelerisque magna eu interdum vulputate. Maecenas gravida eget erat at semper. Phasellus blandit eu purus vel posuere. Donec nisi tortor, vestibulum pretium tortor ac, tempor ultricies velit. Mauris justo arcu, placerat quis erat quis, fringilla iaculis nisl. Pellentesque felis lacus, laoreet in aliquet a, blandit a nisl. Morbi scelerisque arcu sit amet nisi mollis fringilla. Sed libero nisi, porta non congue id, pretium id tellus. Nunc eu lorem nec sem faucibus pellentesque. Vivamus a ante imperdiet, euismod metus et, aliquet nisl. Fusce eu velit ut ligula vulputate pellentesque eu ut tellus. Vestibulum eget ex id orci pharetra placerat. Mauris vel felis auctor, viverra diam nec, mattis ante. Donec tincidunt vitae ex sit amet viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque vitae vestibulum mi. Pellentesque faucibus, risus et mollis posuere, nisi ipsum vestibulum lacus, ac sodales orci purus in massa. Duis eu blandit mauris. Maecenas tincidunt magna ut quam porta luctus. Cras at nunc id tellus rutrum dignissim non ut purus. Etiam mollis enim sit amet nulla gravida, ut fermentum felis lacinia. Nam elementum urna odio, eget finibus ligula malesuada ac. Donec porttitor nulla eget semper vulputate. Quisque mollis maximus volutpat. Ut ultrices sagittis sollicitudin. Nunc vel maximus nisi, sit amet porttitor sem. Nam eleifend posuere turpis sit amet gravida. Pellentesque tristique auctor dictum. Sed molestie quis purus eget aliquet. Sed volutpat, felis ac vulputate volutpat, quam libero porta magna, sit amet viverra ante urna ac nisi. Sed at diam sit amet metus venenatis auctor vel iaculis nulla. Aenean vel egestas nunc, a cursus libero. Pellentesque accumsan nisl orci, rhoncus sollicitudin augue tempus congue. Sed eu velit rutrum, tincidunt justo non, tristique lorem. Nullam auctor mollis lobortis.'
-                            )
-                        ),
+                        this.props.children,
                         React.createElement(
                             'section',
                             { className: 'right' },
