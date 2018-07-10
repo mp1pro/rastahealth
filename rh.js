ffe006215d88e04c012de0d219e0ecee94a718ba
mp1pro <mp1pro@gmail.com>
2018-07-10 16:31:28 -0400

    change require to import part 1 of 2

--- rh.js
+++ rh.js
@@ -1,24 +1,18 @@
-/*
 require('babel-register')({
-	presets: ['env']
+	presets: ['react']
 });
-*/
+const imPORT = require('./config/config');
 
-//const imPORT = require('./config/config');
-import PORT from './config/config';
-const imPORT = PORT;
-
-//const express = require('express');
-import express from 'express';
+const express = require('express');
 const app = express();
+
 app.use(express.static('public'));
 
-import routes from './routes/index.jsx';
-app.use(routes);
+app.use(require('./routes/index.jsx'));
 
-let port = process.env.PORT || imPORT.PORT;
+const PORT = process.env.PORT || imPORT.PORT;
 
-app.listen(port, function(){
+app.listen(PORT, function(){
 	let test= 'testing';
-	console.log(test + ' http://localhost:' + " " + port)
+	console.log(test + ' http://localhost:')
 })
